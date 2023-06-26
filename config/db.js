2fc08308daafbc42cbcd6eb20b5616cfe0b93ff3
iamFaizee <khanfaizan1123@gmail.com>
2023-06-26 22:57:35 +0530

    Backend completed

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const connection = mongoose.connect("mongodb://127.0.0.1:27017/todo_app")
+const connection = mongoose.connect(process.env.MONGO_CONNECTION)
 .then((res) => {
     console.log('connection established')
 })
--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const { authentication } = require('./middleware/authentication');
 
 const app = express();
 
+app.use(cors())
+
 app.use(express.json());
 
 
